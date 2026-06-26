3d156f324e5f167467615f83694ee4d781e31f55
Ikwuka Okoye <ikwukao@proton.me>
2026-06-26 22:04:47 +0100

    chore: test program

--- main.py
+++ main.py
@@ -12,21 +12,36 @@ class HashTable:
             self.collection[hashed_key] = {}
 
         self.collection[hashed_key][key] = value
-        
+
     def remove(self, key):
         hashed_key = self.hash(key)
-        
+
         if hashed_key in self.collection:
             if key in self.collection[hashed_key]:
                 del self.collection[hashed_key][key]
-                
+
                 if not self.collection[hashed_key]:
                     del self.collection[hashed_key]
-                    
+
     def lookup(self, key):
         hashed_key = self.hash(key)
-        
+
         if hashed_key in self.collection:
-            return self.collection[hashed_key].get(key)
-        
+            return self.collection[hashed_key].get(key, None)
+
         return None
+
+
+ht = HashTable()
+
+ht.add("golf", "sport")
+print(ht.collection)
+
+ht.add("dear", "friend")
+ht.add("read", "book")
+print(ht.collection)
+
+print(ht.lookup("golf"))
+
+ht.remove("golf")
+print(ht.lookup("golf"))
