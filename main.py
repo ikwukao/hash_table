b97874e782f56f915edae5c4002a61441cffa634
Ikwuka Okoye <ikwukao@proton.me>
2026-06-26 21:38:56 +0100

    feat: define hash and add method

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 class HashTable:
     def __init__(self):
         self.collection = {}
+        
+    def hash(self, key):
+        return sum(ord(char) for char in key)
+
+    def add(self, key, value):
+        hash_key = self.hash(key)
+
+        if hash_ky not in self.collection:
+            self.collection[hashed_key] = {}
+
+        self.collection[hashed_ky][key] = value
 
